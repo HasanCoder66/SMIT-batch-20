3003cd49e1aeeda7dbb470d857d0037772b87e46
hasancoder66 <hasanashraf874@gmail.com>
2026-02-17 12:51:23 +0500

    js intro and 3 chapters of js

---
+++ JavaScript/class-1-Intro/app.js
@@ -0,0 +1,73 @@
+// console.log("mera name hasan hai..")
+
+// let username = 
+
+
+// console.log(username)
+
+
+
+// Data Types 
+
+// number 12
+// Boolean  true  false
+// undefined
+// string "value" 'string'  `str` => backtick
+
+
+
+// var , let , const 
+
+// var name = "Hasan"
+// let num = 12
+
+// const isLoggedInUser = false
+
+// console.log(isLoggedInUser)
+
+
+
+// var keywords = "value"
+// let num = 12
+
+// console.log("type of keyword -->",typeof keywords)
+// console.log("type of num -->",typeof num)
+
+
+
+// Declare and Initialize
+
+
+
+// var a    ---> declare 
+
+// var a = "kuch bi"  ---> declare and initialize 
+
+// keyword variable name equal value
+
+// var name_
+// // console.log(typeof name_)
+
+// name_  = "Hasan"
+// name_  = "Hasan Ashraf"
+
+
+// console.log(name_)
+
+
+
+
+// var firstName = "Hasan"
+// var lastName = "Ashraf"
+
+
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName)
+
+
+// let num1  = 12;
+
+// let num2 = 12
+
+// console.log(num1 + " " + num2)
---
+++ JavaScript/class-1-Intro/assignment.js
@@ -0,0 +1,9 @@
+// Chapter # 1
+
+
+// Question #1
+// alert("Hello User!")
+
+
+// Question #2
+alert("Welcome to Js Land! \n kuch bi")
