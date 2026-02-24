2ebe1eab5650fb93b13094e66f402c986d9abb94
hasancoder66 <hasanashraf874@gmail.com>
2026-02-24 10:02:15 +0500

    if & else and prompt comparison operator

---
+++ JavaScript/class-4-Prompt-If-&-Else/app.js
@@ -0,0 +1,93 @@
+// let username = prompt("Enter your name","Muhammad Hasan",)
+// console.log("Username ==> ", username)
+
+// window.alert()
+// window.prompt()
+
+
+
+// if keyword parentheses curly brackets
+
+// condition 
+// execution 
+
+// if ka block tab chly ga jab condition true hogi. 
+// else kab chly ga jab if ki condition false hogi 
+
+// if(true){
+//     console.log("if block chl giyaaa -->")
+// }else {
+//     console.log("else block chlaa ==>")
+// }
+
+
+
+// let user = prompt("Enter your name", "hasan")  
+// console.log(user)
+
+// if(user == "Hasan"){ /// block 
+//     console.log("Hello Hasan!")
+// }else {
+//     console.log("Hasan Nhi Milaaa -->")
+// }
+
+
+
+// let userAnswer = prompt("What is Js?", "Javascript is a programming language")
+// let answer = "Javascript is a programming language"
+// let score = 0;
+
+// if(userAnswer == answer){
+//     score++
+//     console.log("User ka total score.", score)
+// }else {
+//     console.log("Wrong Answer!")
+// }
+
+
+
+// let userAge = prompt("Enter Your Age",18);
+
+// if(userAge == 18){
+//     console.log("garri chlaa loo")
+// }else {
+//     console.log("barry hoker aou")
+// }
+
+
+// let message= ""
+
+// if(true){
+// message = "kesy ho"
+// }
+
+// let num = 456768
+// if(num != 45676){
+//     console.log("Try Again")
+// }else {
+//     console.log("you win")
+// }
+
+
+// Comparision Operator 
+
+// == Equal To ==> Data Type check nhi kerta values ki   
+// === Strict Equal To  ==> Data Type check kerta values ki
+// != Not Equal To 
+// !== Strict Not Equal To 
+// > is greater than ==> is sy bara
+// < is less than ==> is sy chota
+// <= is less than or equal  ==> is ke barabar ya is sy chota 
+// >= is greater than or equal ==> is ke barabar ya is sy barra 
+
+
+
+// let password = prompt("Enter Your Password")
+
+// console.log(password.length)
+
+// if(password.length >= 8){
+//     console.log("password length is ok")
+// }else {
+//     console.log("password at least 8 characters")
+// }
