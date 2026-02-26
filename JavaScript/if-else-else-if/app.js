0691d96d6edf1c0bc6903db477f87a29a10608d4
hasancoder66 <hasanashraf874@gmail.com>
2026-02-26 10:06:22 +0500

    if, else, else if and nested if

---
+++ JavaScript/if-else-else-if/app.js
@@ -0,0 +1,86 @@
+// let username = "Wajhi";
+
+// condition  true
+// if(username == "Wajhih"){
+//     console.log("Hello Wajhi!")
+// }else {
+//     console.log("wajhi nhi milaa ==>")
+// }
+
+// if , else if , else
+
+// let purchaseMobileName = "iphone"
+// let availableMobile = "iphone"
+
+// let technoMobile = "Techno"
+
+// if(purchaseMobileName == availableMobile){
+//     console.log("kitny ka hai")
+// } else if(purchaseMobileName == technoMobile) {
+
+// }
+
+//     let guessNum = prompt("Enter a Number", 5)
+// console.log(guessNum)
+
+//     if(guessNum == 0 ){
+//         console.log("arry tukka sahi lag gaya")
+//     }else if(guessNum == 1){
+//         console.log("good hai bhai sahi jarahy ho...")
+//     }else if (guessNum == 5){
+//         console.log("arry wah bhaeee wah win")
+//     }else {
+//         console.log("please enter a valid number!")
+//     }
+
+// if(false){}
+// else if(false){}
+// else {}
+
+// let value = "a"
+// if(value == "A" || value || "a"){
+//     console.log("vowel milgaya ==>")
+// }
+
+// let a = "A";
+
+// let asciiCode = a.charCodeAt()
+// console.log(asciiCode)
+
+// if(asciiCode >= 48 && asciiCode <= 57 ){
+//     console.log("It's a number")
+// } else if (asciiCode >= 65 && asciiCode<= 90){
+//     console.log("It's a Uppercase")
+// }
+
+//     if(2 == 2){
+//         //execution
+//         if(4 < 5){
+//             console.log("four is less then 5")
+//         }
+//     }else {
+// }
+
+// REQUIREMENT ==>
+
+// JS
+// KARACHI
+// 18
+
+let language = prompt("Enter your programming language", "JavaScript");
+
+if (language == "JavaScript") {
+  let city = prompt("Enter Your City Name", "Karachi");
+  if (city == "Karachi") {
+    let age = prompt("Enter Your Age", "18");
+    if (age >= "18") {
+      console.log("aap job ke liye elegible ho.");
+    } else {
+      console.log("barry hoker aou");
+    }
+  } else {
+    console.log("Karachi ajaou");
+  }
+} else {
+  console.log("javascript seekh ke aou");
+}
