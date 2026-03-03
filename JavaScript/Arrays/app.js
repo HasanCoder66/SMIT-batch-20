d1e446174a58efab87994605403c5e2dce2b086f
hasancoder66 <hasanashraf874@gmail.com>
2026-03-03 09:50:11 +0500

    Arrays or it's methods in javascript

---
+++ JavaScript/Arrays/app.js
@@ -0,0 +1,82 @@
+// console.log("js is running -->")
+
+
+// var karachiCity = "Karachi"
+// // var lahoreCity = "Lahore"
+// // var islCity = "Islamabad"
+
+// console.log(karachiCity)
+
+// let cities = ["karachi", "lahore","islamabad","multan","Rawalpindi"] // ==> Reference Data Type 
+// console.log("Welcome to",cities[0])
+
+
+// let students = [];
+// console.log(students)
+
+
+// let array = new Array()
+// console.log(array)
+
+
+// karachiCity = "k City"
+// console.log(karachiCity)
+
+// cities[0] = "k city "
+// console.log(cities)
+
+
+
+// let students = ["shariq", "saleem", "shakoor", "suleman", "fayyan"]
+
+// array ka last Element ko remove kerna hai ==>  kaisy pop()
+// console.log("Before Pop -->",students)
+// let returnVal = students.pop()
+// let returnVal = students.shift()   // first element remove kerta hai
+
+// let returnVal =students.push("Hasan", "Hussain", "Aashir")
+
+// console.log("Return Value of push -->",returnVal)
+// console.log("Array -->",students)
+
+
+
+
+
+
+
+
+// arr[1] = true;
+
+// console.log(arr)
+
+
+
+// let returnVal = arr.unshift("33")
+// console.log(returnVal)
+// console.log(arr)
+
+
+
+// let r = arr.pop()
+// console.log(r)
+// console.log(arr)
+
+
+
+
+// target index, last index
+// let returnVal = arr.slice(-5)
+// console.log(returnVal)
+
+
+// console.log(arr)
+
+
+// let laptopArr = ["dell", "hp","lenovo","toshibha","mac",]
+
+// let returnVal = laptopArr.splice(2, 2)  ==> Remove Elements
+// let returnVal = laptopArr.splice(2, 2, "kuch bi add kerdo", "yele lo") // ==> add
+
+// console.log("Return Value", returnVal)   // ==> ["lenovo","toshibha"]
+// console.log("Update Laptop Array -->",laptopArr)
