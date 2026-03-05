89627bc9564137a2d09c544fc1e32574a7d1ad5f
hasancoder66 <hasanashraf874@gmail.com>
2026-03-05 09:32:59 +0500

    Arrays Complete

--- JavaScript/Arrays/app.js
+++ JavaScript/Arrays/app.js
@@ -80,3 +80,63 @@
 
 // console.log("Return Value", returnVal)   // ==> ["lenovo","toshibha"]
 // console.log("Update Laptop Array -->",laptopArr)
+
+
+let city0 = "Karachi"
+let city1 = "Karachi"
+let city2 = "Karachi"
+
+
+
+
+// let arr = ["Islamabad", city1, city2, false, undefined]
+// console.log(arr)
+
+// 
+
+// let newArr = new Array()
+
+
+let students = ["Fayyan", "Shariq", "Aashir", "Suleman","Umair"];
+// let arr = [];
+// console.log(arr)
+// console.log(students.shift())
+// console.log(students.pop())
+// console.log(students.push("Wajih", "adnan","shakoor"))
+// console.log(students.unshift("Wajih", "adnan","shakoor"))
+
+// let bestStudents = students.slice(0,3)
+
+// 3 arguements
+// let returnVal= students.splice(3,1,"Salman bhai", "hasan","abrar")
+// console.log("splice return ==>", returnVal)
+
+// let returnVal = arr.splice(0,0, "Number","String","Boolean")
+// console.log(returnVal)
+// console.log("Best Students Array ==>",bestStudents )
+// console.log("Students Array ==>",students)
+
+
+
+// let animals = ["cat", "dog","lion","horse","elephant","snake"];
+// let numbers = [9, 21, 4, 2, 7, 5] 
+// let returnVal = animals.reverse()
+// let returnVal = numbers.sort()
+// console.log(returnVal)
+
+
+// let firstName = "Hasan"
+// let lastName = "Ashraf"
+
+// let fullName = firstName.concat(" hasancoder66 ",lastName, "kuchbi")
+// console.log(fullName)
+
+
+
+
+// console.log(students)
+
+// // students.length -1 = "Saim"
+
+// students[3] = "Saim"
+// console.log(students)
