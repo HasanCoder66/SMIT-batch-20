3ffa29f80d15af923ae8ab999ace939681763404
hasanCoder66 <hasanashraf874@gmail.com>
2026-03-07 09:49:44 +0500

    for loop, flag, boolean, array length loop

---
+++ JavaScript/for-loop/app.js
@@ -0,0 +1,78 @@
+// console.log("js file is running -->")
+
+// 
+
+// keyword, round bracket,   curly bracket
+                             // execution   
+
+            
+
+// start , end , change 
+// initialization, condition, increment || decrement 
+
+// for (let i = 1; i <= 10000; i++) {
+//     console.log("hello", i)
+// }
+
+
+// for(let i = 100; i > 0; i--){
+//     console.log("hello", i)
+// }
+
+
+let students = ["Naveed", "Hasan","Fayyan","Shahrukh","Saim","Umair"]
+
+// console.log(students.length)
+
+// console.log(students[0])
+// console.log(students[1])
+// console.log(students[2])
+// console.log(students[3])
+// console.log(students[4])
+// console.log(students[5])
+
+// for(let i = 0; i < students.length; i++){
+//     console.log(students[i])
+// }
+
+
+// let cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu" , "Karachi"];
+
+// let cleanCity = "Great Falls"
+
+// for(let i =0; i < cleanestCities.length; i++){
+//     if(cleanCity == cleanestCities[i]){
+//         console.log("It's a clean city!")
+//     }else {
+//         console.log("clean kerlo city ko!")
+//     }
+// }
+
+
+
+// let users = ["recruiter","canidate","manager","Admin","Super Admin"];
+
+// let isAdmin = "no";
+
+// for(let i = 0; i < users.length; i++){
+//     // console.log("Hamara User ==> ",users[i])
+//     if("Admin" == users[i]){
+//     isAdmin = "yes"
+//     break;
+//     }
+//     console.log(users[i])
+// }
+
+
+// if(isAdmin == "yes"){
+//      console.log("Access Dashboard")
+//         console.log("Allow Access Every where")
+// }
+
+
+
+// for loop table
+let tableNum = 20
+for(let i = 1; i <= 10; i++){
+    console.log(tableNum,"X",i,"=", tableNum * i)
+}
