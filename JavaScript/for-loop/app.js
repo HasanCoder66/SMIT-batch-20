ce90c112ae64c03e330a4f605a80db471d299e94
hasancoder66 <hasanashraf874@gmail.com>
2026-03-10 09:30:28 +0500

    foor loop and nested loop done

--- JavaScript/for-loop/app.js
+++ JavaScript/for-loop/app.js
@@ -72,7 +72,137 @@ let students = ["Naveed", "Hasan","Fayyan","Shahrukh","Saim","Umair"]
 
 
 // for loop table
-let tableNum = 20
-for(let i = 1; i <= 10; i++){
-    console.log(tableNum,"X",i,"=", tableNum * i)
-}
+// let tableNum = 20
+// for(let i = 1; i <= 10; i++){
+//     console.log(tableNum,"X",i,"=", tableNum * i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// ===> REVISION LOOP & NESTED LOOP
+
+// start , end , change 
+
+// vairable initialization, condition; increment || decrement
+
+// for(let i = 0;  i < 5; i++){
+//     console.log("hello!", i)
+// }
+
+
+// let items = ["samosa","roll","paratha","chatni","kachori","lazania","kathiya wari choly"
+// ];
+// console.log(items)
+
+// console.log(items[0])
+// console.log(items[1])
+// console.log(items[2])
+
+
+// for (let i = 0; i < items.length; i++) {
+//     console.log(items[i])
+// }
+
+
+
+// let users = ["hasan", "fayyan","sharik"];
+
+// for(let i = 0; i < users.length; i++){
+//     console.log(users[i])
+// }
+
+
+
+// let tableNum = 20;
+
+// for(let i = 1; i<=10; i++){
+//     console.log(tableNum, "X" , i, "=", tableNum * i)
+// }
+
+// console.log(tableNum, "X" , 2, "=", tableNum * 2)
+// console.log(tableNum, "X" , 3, "=", tableNum * 3)
+// console.log(tableNum, "X" , 4, "=", tableNum * 4)
+// console.log(tableNum, "X" , 5, "=", tableNum * 5)
+// console.log(tableNum, "X" , 6, "=", tableNum * 6)
+// console.log(tableNum, "X" , 7, "=", tableNum * 7)
+// console.log(tableNum, "X" , 8, "=", tableNum * 8)
+// console.log(tableNum, "X" , 9, "=", tableNum * 9)
+// console.log(tableNum, "X" , 10, "=", tableNum *10)
+
+
+
+
+let multiDimensionalArray = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+// console.log(multiDimensionalArray[0][2])
+// console.log(multiDimensionalArray[0][2])
+
+
+
+// for (let i = 0; i < multiDimensionalArray.length; i++){
+// // console.log(multiDimensionalArray[i])
+
+// for(let j = 0 ; j < multiDimensionalArray[i].length; j++){
+//     console.log(multiDimensionalArray[i][j])
+// }
+// }
+
+
+
+// let studentNamesArr = [["fayyan","hasan"], ["shahrukh","naveed"]]
+
+// for(let i = 0 ; i < studentNamesArr.length; i++){
+    
+//     for(let j = 0; j < studentNamesArr[i].length; j++){
+//         console.log(studentNamesArr[i][j])
+//     }
+// }
+
+
+
+
+// for(let i = 0; i < 3; i++){
+
+//     /// 
+//     // console.log(i)
+//     // console.log("hey hasan")
+
+
+
+// // for(let j = 0; j < 5; j++){
+// //     console.log("i ==>", i, "j ==>",j)
+// // }
+
+
+
+
+
+
+
+//     ////
+// }
+
+
+
+
