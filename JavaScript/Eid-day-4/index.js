19e760751bb75789df2a00b16a57c9bf35a3e1b6
hasancoder66 <hasanashraf874@gmail.com>
2026-03-24 12:48:10 +0500

    Revision

---
+++ JavaScript/Eid-day-4/index.js
@@ -0,0 +1,99 @@
+// let username = "Asghar"
+// let num = 12;
+
+// // camelCase tm use kersekty ho special character $ Or _
+
+// console.log(username)
+
+// let asgharKaiBabu = "Iqbal"
+
+// // console.log(username + " "+asgharKaiBabu)
+
+// console.log(username.concat(" ",asgharKaiBabu))
+
+// let eidDate = "21 Mar 2026"
+
+// if(eidDate == "22 Mar 2026"){
+// console.log("Eid Mubarak!")
+// }
+// else if(eidDate == "23 Mar 2026"){
+// console.log("Eid Mubarak!")
+// }
+// else {
+// console.log("Eid Mubarak!, else walaa!")
+// }
+
+// Job Requirements
+
+// Pakistani , 18 , intermediate, exp = 2,
+
+// if(userCountry == "Pakistan"){
+
+//     if(userAge == 18){
+//         console.log("age 18 hai")
+//     }else{
+//         console.log("age bari hai")
+//     }
+// }
+
+
+
+// let userCountry = "Pakistan";
+// let userAge = 16;
+// let userQualification = "Intermediate";
+// let userExperience = 2;
+
+// if (
+//   userCountry == "Pakistan" &&
+//   userAge >= 23 &&
+//   userQualification == "Intermediate" &&
+//   userExperience == 2
+// ) {
+//   if(userAge <= 28){
+//     console.log("aap is job ke liye elegible hain!");
+//   }
+// }
+
+
+
+// let arr = ["Mannan","Hannan","Arman"]
+
+// let joinVal = arr.join(" ")
+
+// let splitVal = joinVal.split(" ")
+
+// splitVal[0] = "Mannan Bilal"
+// splitVal[1] = "Hannan Bilal"
+// splitVal[2] = "Arman Ismail"
+
+
+
+// console.log(splitVal)
+
+
+// arr.unshift(122)
+// arr.push(122,"256")
+// arr.shift()
+// arr.pop()
+
+// arr.reverse()
+// arr.sort()
+// arr.splice(0, arr.length,"Hasan")
+// let returnVal = arr.join(Math.round(Math.random() * 10))
+// let returnVal = arr.join(" ")
+
+// console.log(returnVal.split(" "))
+// console.log("Actual Array ==>",arr)
+// let returnVal = arr.slice(0,3)
+
+// console.log(returnVal.sort())
+
+
+let oneArr = ["hasan","ashraf"]
+// console.log(oneArr)
+
+let twoArr = oneArr
+twoArr.push("Mannan")
+
+console.log(oneArr)
+
