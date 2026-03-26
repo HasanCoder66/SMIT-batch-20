dec6217b81a564c62a5e521fd1ed7821293c32c3
hasancoder66 <hasanashraf874@gmail.com>
2026-03-26 12:38:46 +0500

    Revision and chapter 21to 25

--- JavaScript/for-loop/app.js
+++ JavaScript/for-loop/app.js
@@ -165,7 +165,7 @@ let multiDimensionalArray = [
 
 // console.log("Copy ki -->",sliceArr)
 
-let strArr = ["kuchbi", "none", "bekar", "ali", "hasan"];
+// let strArr = ["kuchbi", "none", "bekar", "ali", "hasan"];
 
 // let emptyArr = []
 
@@ -195,12 +195,123 @@ let strArr = ["kuchbi", "none", "bekar", "ali", "hasan"];
 // }
 
 
-for(let i = 100 ; i > 0; i--){
+// for(let i = 100 ; i > 0; i--){
     
-    for(let j = 10 ; j > 0; j--){
+//     for(let j = 10 ; j > 0; j--){
     
-    console.log(i, j)
+//     console.log(i, j)
    
-}
+// }
    
-}
+// }
+
+
+
+// for(let i = 1; i < 11; i++){
+//   console.log(i * 8)
+// }
+
+
+// for (let i = 10 ; i >= 1 ; i--){
+//   console.log(i)
+// }
+
+
+
+// statring ,      ending   , changes 
+// initialization, condition, increment | decrement
+
+
+// let requriredUserMila = false;
+
+// let studentsArr = [
+// "shariq", "hassan","mubasshir"
+// ]
+
+
+// for (let i = 0 ; i < studentsArr.length; i++){
+//   console.log(studentsArr[i])
+//   if(studentsArr[i] == "hassan"){
+//     requriredUserMila = true
+//     break
+//   }
+// }
+
+
+// if(requriredUserMila == true){
+//   console.log("user mera mil gayaa!")
+// }
+
+
+
+
+
+
+let twoDarray = [
+  ["azeem","shakoor"],[12,34]
+]
+
+// console.log(twoDarray[0][1].toUpperCase())
+
+
+
+// let firstName = "muhammad"
+// let lastName = "hasan"
+
+
+// let fullName = firstName.slice(0,1).toUpperCase() + firstName.slice(1,8) + " " + lastName.slice(0,1).toUpperCase() + lastName.slice(1)
+
+// console.log(fullName)
+
+// console.log(firstName.toUpperCase())
+
+
+
+// let paragraph = "my name is salman"
+
+
+// let arr = paragraph.split(" ")
+// let capitalizeArr = []
+
+
+// // console.log(arr)
+
+// for(let i = 0; i < arr.length; i++){
+
+//   let titleCaseWords = arr[i][0].toUpperCase() + arr[i].slice(1)
+//   capitalizeArr.push(titleCaseWords)
+  
+// }
+
+// let joinReturn = capitalizeArr.join(" ")
+// paragraph = joinReturn
+
+// console.log(paragraph)
+
+
+
+
+let randomPara = "suleman hai abdul hai abdul"
+
+
+// for(let i =0 ; i < randomPara.length; i++){
+  
+// if(randomPara.slice(i, i+8) == "abdullah"){
+//   console.log("abdullah milgayaa!")
+//   break;
+// }
+// }
+
+
+console.log(randomPara.lastIndexOf("abdul"))
+
+
+
+
+// let arrayHai = ["12",45,56]
+// console.log(arrayHai[0].charAt(arrayHai[0].length - 1))
+
+
+// randomPara = randomPara.replace(/abdul/g,"abdullah")
+
+// console.log(randomPara)
