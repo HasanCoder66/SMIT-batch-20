1437a9af5902a54743e5c29fdd0d72a84ef336e3
hasancoder66 <hasanashraf874@gmail.com>
2026-04-07 12:39:50 +0500

    function | generate password logic

---
+++ JavaScript/Functions/app.js
@@ -0,0 +1,114 @@
+// Functions ==> ?
+
+
+
+// function sum (){
+
+//     console.log(12 + 12)
+// }
+
+
+// sum()
+// sum()
+
+
+// function greet(username){
+// console.log("Hello User! " + username)
+// }
+
+// greet("Muhammad Hasan") /// arguements
+
+
+// console.log("Hello")
+
+
+
+// keyword | function name | parentheses or round brackets | curly brackets
+
+
+// function foo (){  // block of code
+// function body || execution | logic | implentation
+
+// }
+
+
+// block of code kab execute hota hai ?
+// jab aap function ko call kerty hai ya phir invoke
+
+
+// get time function
+
+
+// function getTime () {
+//     let now = new Date()
+
+//     let getHour = now.getHours()
+//     let getMins = now.getMinutes()
+
+//     console.log("Current Time: " + getHour + ":"+ getMins)
+// }
+
+// getTime()
+
+
+
+
+// // parentheses | parameters 
+// function abc (n1, n2,n3) {
+// console.log(n3)
+// }
+
+
+// abc(12,33,44) //parentheses | arguements
+
+
+
+
+    // function foo(a,b,c){
+    // console.log("Hello "+ a)
+    // }
+
+    // let username = "Muhammad hasan"
+    // foo(username)
+
+
+
+    // let result = 
+
+
+
+    // function abc (){
+    //     return "Hasan"
+    // }
+
+
+    // let username = abc()
+    // console.log(username + " " + "Ashraf")
+
+
+
+// Return Examples ==> 
+// let arr = [1,2,3]
+
+// let pushReturnVal = arr.push("jani")
+
+// console.log(pushReturnVal + 6)
+
+
+let paragraph = document.getElementById("showPass")
+console.log(paragraph)
+let passCharacters = "@#$%^&*()ASDFjklgasdfGHJ45asdafd465KLQWER12345TYU"
+
+
+function randomGeneratePassword (){
+    let password = ""
+for(let i =0 ; i < 8; i++){
+    let randoNum = Math.floor(Math.random() * passCharacters.length)
+    // password = password + passCharacters[randoNum]
+    password += passCharacters[randoNum]
+}   
+
+paragraph.innerHTML = password
+}
+
+// randomGeneratePassword()
---
+++ JavaScript/Revision-2-4-26/app.js
@@ -0,0 +1,47 @@
+// let const var 
+
+
+
+var a = "hasan"
+
+// Primitives 
+// Reference || Non Primitives
+
+
+// Primitives ==> string, number, boolean, null, undefined, bigint, symbol
+// Reference  ==> (), [], {}
+
+
+// var num = 45
+// let isAdmin = false;
+
+
+
+
+// let n1 = 78;
+// let n2 = 22
+
+// console.log(n1 + n2)
+
+
+// let firstName = "Muhammad"
+// let lastName = "Ali"
+
+// let fullName = firstName +" " +lastName
+// console.log(fullName)
+
+
+// let age = 19;
+
+// if(age == 18){
+//     console.log("Elegible for vote")
+// } else if(age < 18 ){
+//     console.log("Not Elegible for vote")
+// }else {
+//     console.log("jabh koi condition true nhi ho gi to kia clhy ga else ok")
+// }
+
+
+let arr = [1,2,"string","hasan",alert]
+
+console.log(arr[1])
