42234c210c05aa809a475a94fa4a47ee157ebcc1
hasancoder66 <hasanashraf874@gmail.com>
2026-04-09 12:49:40 +0500

    Revision Functions ch 40 done

---
+++ JavaScript/Functions/Revision/app.js
@@ -0,0 +1,193 @@
+// console.log("javascript is running!")
+
+
+
+// Normal function
+
+// function parmeters 
+// function multiply (a,b, c) {
+// console.log(a * b)
+// }
+
+
+// // function arguements 
+// multiply(78,1)
+
+
+
+// let score = 0;
+
+// let userAns1 = "Hyper Text Markup Language"
+// let correctAns = "Hyper Text Markup Language"
+
+// function scoreUpdate (){
+
+//     if(userAns1 == correctAns){
+//         score++
+//     }
+
+// }
+
+// scoreUpdate()
+// console.log(score)
+
+
+// let users = [
+//     {
+//   email : "codermhasan@gmail.com",
+//  pass : "codermhasan"  
+// }
+// ]
+
+// let email = "codermhasan@gmail.com"
+// let pass = "codermhasan"
+
+// let isLoggedIn = false;
+
+// function loginHandler () {
+
+//     if(users[0].email == email && users[0].pass == pass){
+//         isLoggedIn = true
+//          return "User login successfully"
+//         console.log("Welcome Back! Dashboard")
+//     } else {
+//         console.log("Invalid Email or Password")
+//     }
+
+   
+// }
+
+// console.log(loginHandler())
+
+
+
+
+// function foo (){
+//     let a = 45;
+//     let b = 65
+
+//     let result = b - a
+
+//     console.log("hello")
+//     return 
+// }
+
+
+// let fooReturn = foo()
+// console.log(fooReturn)
+
+
+
+// function abc (){
+// let a = "ABC"
+// let z = "XYZ"
+
+// console.log(a)
+
+// return 
+
+// console.log(z)
+// }
+
+// console.log(abc())
+
+
+
+// let sat = "saturday"
+// let sun = "sunday"
+// let fri = "Friday"
+
+
+// if(sun == "sunday" || fri == "Friday"){
+//     console.log("class ki chutti hai..")
+// }
+
+
+// let day = "Tuesday"
+
+// switch (day) {
+//  case "Thursday":
+//             console.log("class anaa hai...")
+//             break;
+//     case "Monday":
+//         console.log("class ki chutti hai")
+//         break;
+//         case "Tuesday":
+//             console.log("class anaa hai...")
+//             break;
+
+//             default :
+//             console.log("kuch bi likh diyaaa")
+       
+
+// }
+
+
+
+
+// let user = "user"
+
+// switch (user) {
+//  case "user":
+//             console.log("User Dashbaord")
+//             break;
+//     case "admin":
+//         console.log("Admin Dashboard")
+//         break;
+//         case "recruiter":
+//             console.log("Recruiter Dashbaord")
+//             break;
+
+//             default :
+//             console.log("kuch bi likh diyaaa")
+       
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let days = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday"]
+
+// let currentDate = new Date()
+// console.log()
+
+// let day = days[currentDate.getDay()]
+
+// switch (day) {
+//  case "Thursday":
+//             console.log("class anaa hai...")
+//             break;
+//     case "Monday":
+//         console.log("class ki chutti hai")
+//         break;
+//         case "Tuesday":
+//             console.log("class anaa hai...")
+//             break;
+
+//             default :
+//             console.log("kuch bi likh diyaaa")
+       
+
+// }
+
+
+
+// for(let i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+
+
+let arr = [12,22]
+
+arr.splice(0,1, 122)
+
+console.log(arr)
