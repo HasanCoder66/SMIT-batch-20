825ebf73fd0d351d8fd0481873573aa1af199cb2
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-04-11 11:52:20 +0500

    chapter 41 to 44 done

---
+++ JavaScript/scripts-commenting/app.js
@@ -0,0 +1,27 @@
+
+/* let para = "han jani kia haal hian?"
+console.log("hello world!") */
+
+
+// for loop
+
+
+// start ,               end,           change 
+
+// var initailization , condition , increment or decrement 
+
+
+// for(let i = 0;  i < 5; i++){
+//     console.log(i)
+// }
+
+
+
+// while loop bolta hai  jo tmny theory for loop mai parhi hai wohi hon.
+
+// let i = 0; 
+
+// do {
+//     console.log(i)
+//     i++
+// }while(i < 3)
