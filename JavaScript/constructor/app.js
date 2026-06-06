d82d12346f5f12a43e5be975420079669665cb8d
hasanCoder66 <hasanashraf874@gmail.com>
2026-06-06 12:23:40 +0500

    constructors done

--- JavaScript/constructor/app.js
+++ JavaScript/constructor/app.js
@@ -1,35 +1,136 @@
-let user = {
-  name: "fayyan",
-  age: 25,
-  email: "qwertyyo@gmail.com",
-  password: "12345678",
-};
+// constructor for methods
 
-let user2 = {
-  name: "shahrukh",
-  age: 25,
-  email: "qwertyyo@gmail.com",
-  password: "12345678",
-};
+function Student (name, email, password){
+  this.name = name;
+  this.email = email;
+  this.password = password;
 
-let usersArr = [
-  {
-    name: "shahrukh",
-    age: 25,
-    email: "qwertyyo@gmail.com",
-    password: "12345678",
-    printUserName : function (){
-        // console.log(this.name);
-        return usersArr[0].age
+  // this.printStudentDetails = function (){
+  //   console.log("student name -->", this.name);
+  //   console.log("student email -->", this.email);
+  //   console.log("student password -->", this.password);
+    
+  // }
+}
+
+  Student.prototype.printStudentDetails =  function (){
+     console.log("student name -->", this.name);
+     console.log("student email -->", this.email);
+     console.log("student password -->", this.password);
+     
     }
-  },
-  {
-    name: "fayyan",
-    age: 25,
-    email: "qwertyyo@gmail.com",
-    password: "12345678",
-  },
-];
+
+
+let s1 = new Student("ali","ali@gmail.com", '123456789')
+let s2 = new Student("fayyan hussain","fayyan@gmail.com", '123456789')
+let s3 = new Student("rizwan","rizwan@gmail.com", '123456789')
+
+
+
+// s1.printStudentDetails()
+// s3.printStudentDetails()
+
+
+console.log("s1 -->", s1, s1.hasOwnProperty("printStudentDetails"));
+
+console.log("s1 --> ", "printStudentDetails" in s1 );
+// console.log(s2, "s2");
+
+
+
+// function Abc (username){
+// this.username = username
+// }
+
+
+// let a1 = new Abc("shakoor")
+
+// console.log(a1);
+
+
+// function Human (name, gender, age, weight){
+// this.name = name;
+// this.gender = gender;
+// this.age = age;
+// this.weight = weight;
+// }
+
+
+// let h1 = new Human("aslam", "male", 0, "2kg")
+// let h2 = new Human("Humaira", "female", 0, "3kg")
+
+
+// console.log("Human 1 -->", h1);
+// console.log("Human 2 -->", h2);
+
+
+
+// function MobileMaker (brand, model, ram, rom, screenSize, colors, price){
+// this.brand = brand;
+// this.model = model;
+// this.ram = ram;
+// this.rom = rom;
+// this.screenSize = screenSize;
+// this.colors = colors;
+// this.price = price;
+// }
+
+
+// let m1 = new MobileMaker("samsung", "A35", "8GB", "128GB", "5.5 inch", ["black","white","golden"], 109000)
+// let m2 = new MobileMaker("iphone", "xr", "12GB", "256GB", "4 inch", ["black","white","golden"], 55000)
+
+// console.log("M1 ==>", m1);
+// console.log("M2 ==>", m2);
+
+
+
+// function shirtMaker (brand, type, sizes, colors, price){
+// this.brand = brand;
+// this.type = type;
+// this.sizes = sizes;
+// this.colors = colors;
+// this.price = price;
+// }
+
+
+// let s1 = new shirtMaker("j.", "chkeck", ["s","m","l","xl","xxl"], ["black", "white", "navy blue", "olive green", "bottle green"], 1200)
+// let s2 = new shirtMaker("j.", "formal", ["s","m","l","xl","xxl"], ["black", "white", "navy blue", "olive green", "bottle green"], 2000)
+
+// console.log("s2 --> ", s2);
+
+
+// let user = {
+//   name: "fayyan",
+//   age: 25,
+//   email: "qwertyyo@gmail.com",
+//   password: "12345678",
+// };
+
+// let user2 = {
+//   name: "shahrukh",
+//   age: 25,
+//   email: "qwertyyo@gmail.com",
+//   password: "12345678",
+// };
+
+// let usersArr = [
+//   {
+//     name: "shahrukh",
+//     age: 25,
+//     email: "qwertyyo@gmail.com",
+//     password: "12345678",
+//     printUserName : function (){
+//         // console.log(this.name);
+//         return usersArr[0].age
+//     }
+//   },
+//   {
+//     name: "fayyan",
+//     age: 25,
+//     email: "qwertyyo@gmail.com",
+//     password: "12345678",
+//   },
+// ];
 
 
 
@@ -109,26 +210,26 @@ let usersArr = [
 
 
 
-function Student (name){
-    this.stu = name
-    this.printStuName = function (){
-console.log(this.stu);
+// function Student (name){
+//     this.stu = name
+//     this.printStuName = function (){
+// console.log(this.stu);
 
-const studentName = this.stu
+// const studentName = this.stu
 
-if(!studentName.trim()){
-    console.log("please enter a name");
+// if(!studentName.trim()){
+//     console.log("please enter a name");
     
-}
+// }
 
 
-    }
-}
+//     }
+// }
 
 
 
-let s1 = new Student("")
+// let s1 = new Student("")
 
-console.log(s1);
+// console.log(s1);
 
-s1.printStuName()
+// s1.printStuName()
