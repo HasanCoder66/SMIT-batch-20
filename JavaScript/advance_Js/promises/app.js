f1efcd9c2b111f157d862eb8b1d5cb80d5882c77
hasanCoder66 <hasanashraf874@gmail.com>
2026-07-18 11:51:26 +0500

    js quiz 4 and promises revision

--- JavaScript/advance_Js/promises/app.js
+++ JavaScript/advance_Js/promises/app.js
@@ -1,18 +1,29 @@
 // synchronus code
-fetch(`https://dummyjson.com/products`)
-.then((response) => {
-    // console.log(response);  
- return response.json()  
-})
-.then((result) => {
-    // console.log(result.products);
+// function apiCall (id = 1){
+// fetch(`https://dummyjson.com/products/${id}`)
+// .then((response) => {  
+//  return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((err) => {
+//   console.log(err);
+  
+   
+// })
+// }
 
-    result.products.map((item, idx) => {
-        console.log(item);
+// apiCall(10)
+// .then((result) => {
+//     // console.log(result.products);
+
+//     result.products.map((item, idx) => {
+//         console.log(item);
         
-    })
+//     })
     
-})
+// })
 
 
 // console.log("hello");
