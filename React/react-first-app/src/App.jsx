d4e5941494705bf394614d2fdad8dce01f79cbbf
hasancoder66 <hasanashraf874@gmail.com>
2026-08-04 12:19:43 +0500

    react first app

---
+++ React/react-first-app/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+const App = () => {
+return(
+ <nav className="navbar">
+
+  <h3>Logo</h3>
+
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </ul>
+ </nav>
+)
+}
+
+export default App
