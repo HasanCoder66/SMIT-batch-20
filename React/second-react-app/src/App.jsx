c0642c76b983a0afde864912a6b7b4f9173a7356
hasanCoder66 <hasanashraf874@gmail.com>
2026-08-06 12:55:26 +0500

    second react class

---
+++ React/second-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from './components/Card';
+import "./App.css"
+
+const App = () => {
+  console.log("hello");
+  
+  return (
+      <>
+      {/* navbar */}
+      {/* section */}
+      <section>
+
+        <h2>This is Featured Products Section</h2>
+
+    <Card />
+      </section>
+
+    {/* footer  */}
+      </>
+  )
+}
+
+export default App
---
+++ React/second-react-app/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Card = () => {
+    return (
+        <div className="" style={{
+            padding:"10px",
+            borderRadius:"10px",
+            width:"300px",
+            height:"150px"
+        }}>
+            <h2>This is a Title</h2>
+            <p>this is a description of my card Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ipsa ipsam mollitia!
+            </p>
+        </div>
+    )
+}
+
+
+export default Card
