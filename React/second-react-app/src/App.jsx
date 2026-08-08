60a0d782375745016013615306f11e8fade5c613
hasanCoder66 <hasanashraf874@gmail.com>
2026-08-08 12:15:04 +0500

    react practices

--- React/second-react-app/src/App.jsx
+++ React/second-react-app/src/App.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
-import Card from './components/Card';
-import "./App.css"
+import React from "react";
+import Card from "./components/Card";
+import "./App.css";
 
 const App = () => {
-  console.log("hello");
-  
+  // console.log("hello");
+
+  let isLoggedInUser = false;
+
+  let users = [
+    {id : "00001", name: "abdullah", age: 22 },
+    {id : "00002", name: "janii", age: 25 },
+  ];
+
+
+  let str = ["hello", "kesy ho..."]
+  // if (!isLoggedInUser) {
+  //   return <h1>Please Login...</h1>;
+  // }
+
   return (
-      <>
+    <>
+      {/*mapping */}
+
+{str.map((item, idx) => (
+  <p key={idx}>{item}</p>
+) )}
+
+
       {/* navbar */}
       {/* section */}
       <section>
-
         <h2>This is Featured Products Section</h2>
 
-    <Card />
+        {/* <h2>{abc}</h2> */}
+        {console.log(users)}
+       {users && users.length > 0 && users.map((u, i) => (
+         <Card data={u} key={i}/>
+       ))}
       </section>
 
-    {/* footer  */}
-      </>
-  )
-}
+      {/* footer  */}
+    </>
+  );
+};
 
-export default App
+export default App;
--- React/second-react-app/src/components/Card.jsx
+++ React/second-react-app/src/components/Card.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+// style={{
+//             padding:"10px",
+//             borderRadius:"10px",
+//             width:"300px",
+//             height:"150px"
+//         }}
 
+const Card = ({data}) => {
 
-const Card = () => {
+    console.log(data);
+    
+    let status = "pending";
+
     return (
-        <div className="" style={{
-            padding:"10px",
-            borderRadius:"10px",
-            width:"300px",
-            height:"150px"
-        }}>
-            <h2>This is a Title</h2>
-            <p>this is a description of my card Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ipsa ipsam mollitia!
+        // <div className={`${status ? "bg-green" : "bg-red p-5"}`} >
+        <div className={`${status == "pending" ? "bg-yellow" : `${status ? "bg-green" : "bg-red"} `}`} >
+            <h2>{data.name}</h2>
+            <p>{data.age}
             </p>
+
+            <button>{`${status == true ? "Ok" : `${status == "pending" ? "loading": "kuch biii"}`}`}</button>
         </div>
     )
 }
 
 
 export default Card
+
+
+
+// ternary operator
+
+// condition ? pehla wala: 
+
+// status ? "bg-green" : ""
