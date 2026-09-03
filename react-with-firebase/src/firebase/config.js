33099dc0eaf3c379a84ded223dbf178f28290fd2
hasanCoder66 <hasanashraf874@gmail.com>
2026-09-03 13:03:18 +0500

    changes in code

--- react-with-firebase/src/firebase/config.js
+++ react-with-firebase/src/firebase/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "react-practice-b-20.firebaseapp.com",
   projectId: "react-practice-b-20",
   storageBucket: "react-practice-b-20.firebasestorage.app",
